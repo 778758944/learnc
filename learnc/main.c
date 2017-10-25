978706ea2c6320a17f042eed09144a5171ee568c
xiaobo <xiaobo.xwt@alibaba-inc.com>
2017-10-25 22:50:39 +0800

    Initial Commit

---
+++ learnc/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  learnc
+//
+//  Created by xiaobo on 2017/10/25.
+//  Copyright © 2017年 xiaobo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
